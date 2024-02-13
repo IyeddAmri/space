a3813b0cdb3f2ed5149fa1fe8039300890bf6a0b
IyeddAmri <iyedamri21@gmail.com>
2024-02-13 09:36:43 +0100

    back and front running

---
+++ backend/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define a simple route
+
+
+// Define more routes as needed
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
